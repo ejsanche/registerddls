8632af4939fac96d408c608ac08d96c570ca5c17
Christopher Malone <clmalone@email.uark.edu>
2017-04-05 14:22:08 -0500

    8.1 and 8.2
    completed 8.1 and mostly completed 8.2

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -1,57 +1,70 @@
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE product (
-  id uuid NOT NULL,
-  lookupcode character varying(32) NOT NULL DEFAULT(''),
-  count int NOT NULL DEFAULT(0),
-  createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT product_pkey PRIMARY KEY (id)
+	price numeric CHECK (price > 0)                          /* item price cannot be zero or less */
+	status boolean,
+	id uuid NOT NULL,
+	lookupcode character varying(32) NOT NULL DEFAULT(''),
+	quantity int NOT NULL DEFAULT(0),
+	createdon timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
-  OIDS=FALSE
+	OIDS=FALSE
 );
 
 CREATE INDEX ix_product_lookupcode
-  ON product
-  USING btree
-  (lower(lookupcode::text) COLLATE pg_catalog."default");
+	ON product
+	USING btree
+	(lower(lookupcode::text) COLLATE pg_catalog."default");
 
 INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode1'
-     , 100
-     , current_timestamp
+	uuid_generate_v4()
+	, 'lookupcode1'
+	, 100
+	, current_timestamp
 );
 
 INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode1'
-     , 125
-     , current_timestamp
+	uuid_generate_v4()
+	, 'lookupcode1'
+	, 125
+	, current_timestamp
 );
 
 INSERT INTO product VALUES (
-       uuid_generate_v4()
-     , 'lookupcode3'
-     , 150
-     , current_timestamp
+	uuid_generate_v4()
+	, 'lookupcode3'
+	, 150
+	, current_timestamp
 );
 
 CREATE TABLE employee (
-  id uuid NOT NULL,
-  employeeid character varying(32) NOT NULL DEFAULT(''),
-  firstname character varying(128) NOT NULL DEFAULT(''),
-  lastname character varying(128) NOT NULL DEFAULT(''),
-  password character varying(512) NOT NULL DEFAULT(''),
-  active boolean NOT NULL DEFAULT(FALSE), 
-  classification int NOT NULL DEFAULT(0),
-  managerid uuid NOT NULL,
-  createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT employee_pkey PRIMARY KEY (id)
+	id uuid NOT NULL,
+	employeeid character varying(32) NOT NULL DEFAULT(''),
+	firstname character varying(128) NOT NULL DEFAULT(''),
+	lastname character varying(128) NOT NULL DEFAULT(''),
+	password character varying(512) NOT NULL DEFAULT(''),
+	active boolean NOT NULL DEFAULT(FALSE),
+	classification int NOT NULL DEFAULT(0),
+	managerid uuid NOT NULL,
+	createdon timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT employee_pkey PRIMARY KEY (id)
 ) WITH (
-  OIDS=FALSE
+	OIDS=FALSE
 );
 
 CREATE INDEX ix_employee_employeeid
-  ON employee
-  USING hash(employeeid);
+	ON employee
+	USING hash(employeeid);
 
+CREATE TABLE transaction_table (                                         /* transaction is apparently a key word */
+	record_id numeric NOT NULL,
+	cashier_id character varying (32) references employee(employeeid),
+	total_amount numeric CHECK (price > 0),                              /* cost should never be less than or equal to zero */
+	transaction_type enum('return', 'sale') NOT NULL DEFAULT('sale'),    /* sales are probably more common */
+	reference_id numeric NOT NULL,                                       /* should reference something, but that table doesn't exist yet */
+	created_on timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT primary_record PRIMARY KEY (record_id)
+) WITH (
+	OIDS=FALSE
+);
